2b46a421a5955bdae140a78f9623356323d5d685
Shao <shao@ischool.berkeley.edu>
2013-09-19 19:46:15 -0700

    [Added] madlib.js file with validator function

---
+++ public_html/madlib/madlib.js
@@ -0,0 +1,78 @@
+function validator() {
+
+	//validate name input
+	if ($("#your_name").val().length > 0) {
+		$("#output_name").text($("#your_name").val());
+		$("#output_name_1").text($("#your_name").val());
+		$("#output_name_2").text($("#your_name").val());
+		$("#output_name_3").text($("#your_name").val());
+
+	} else {
+		alert("Please enter your name.");
+		return false;
+	}
+	
+	//validate title input
+	if ($("#title input[type='radio']:checked").val().length > 0) {
+		$("#output_title").text($("#title input[type='radio']:checked").val());
+
+	} else {
+		alert("Please select your title.");
+		return false;
+	}
+
+
+	//validate reaction
+	if ($("#reaction option:selected").val().length > 0) {
+		$("#output_reaction").text($("#reaction option:selected").val());
+	} else {
+		alert("Please choose the reaction when you see a cockroach!");
+		return false;
+	}
+
+
+	//validate occupation
+	if ($("#occupation").val().length > 0) {
+		$("#output_occupation").text($("#occupation").val());
+
+	} else {
+		alert("Please enter your occupation.");
+		return false;
+	}
+
+
+	//validate year
+	if ($("#year").val().match('^[12][0-9]{3}$')) {
+		$("#output_year").text($("#year").val());
+	} else {
+		alert("Please enter a valid year!");
+		return false;
+	}
+
+
+	//validate monster
+	if ($("#godzilla").is(":checked")) {
+		$("#output_godzilla").text($("#godzilla").val());
+	} 
+	
+	if ($("#kingkong").is(":checked")) {
+		$("#output_kingkong").text($("#kingkong").val());
+	}
+	
+	if ($("#mothra").is(":checked")) {
+		$("#output_mothra").text($("#mothra").val());
+	}
+	
+	if ($("#gamera").is(":checked")) {
+		$("#output_gamera").text($("#gamera").val());
+	}
+	
+
+	// show madlib-output
+	$("#madlib-output").show();	
+	return false;
+}
+
+$(document).ready(function() {
+	$("#madlib-input").submit(validator);
+});
